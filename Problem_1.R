e03d3b5a6798068fd1ffc02d268de12e750b02b5
Magdalena <m._.velikova@abv.bg>
2018-03-22 20:04:21 +0200

    Homework1

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,18 @@
-
+#a)
+x<-c(4,1,1,4)
+#b)
+y<-c(1,4)
+# c) - since y is shorter than x, the elements of y are repeated until y becomes the same length as x and this can only happen if the longer
+# vector's length is a multiple of the shorter vector's legth. We get x(4,1,1,4)-y(1,4,1,4)=z(3,-3,0,0)
+z <- x-y
+#d)
+s<-c(x,y)
+#e)
+rep(s,10)
+#f)
+rep(s,3)
+#g)
+seq(7,21)
+7:21
+#h)
+length(seq(7,21))
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,35 @@
-
+#a)
+xmin<-c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax<-c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+#b)
+xmax - xmin
+#c)
+avgmin<-mean(xmin)
+avgmax<-mean(xmax)
+#d)
+xmin[xmin<avgmin]
+#e)
+xmin[xmax>avgmax]
+#f)
+names(xmin)<-c('03Mon18', '04Tue18', '05Wed18', '04Thu18', '05Fri18', '06Sat18', '07Sun18')
+names(xmax)<-c('03Mon18', '04Tue18', '05Wed18', '04Thu18', '05Fri18', '06Sat18', '07Sun18')
+#g)
+temperatures<-data.frame(xmin,xmax)
+#h)
+temperatures<-within(temperatures,
+                     {xminFahrenheit=xmin*9/5+32}
+                     )
+#i)
+temperaturesFahrenheit <- data.frame(
+  Max_temp_Fahr = xmax*9/5 + 32,
+  Min_temp_Fahr = xmin*9/5 + 32
+)
+#j)
+MonFri_temperaturesFahrenheit<-data.frame(
+  Max_temp_Fahr=xmax[seq(1:5)]*9/5+32, 
+  Min_temp_Fahr=xmin[seq(1:5)]*9/5+32
+)
+MonFri_temperaturesFahrenheit<-data.frame(
+  Max_temp_Fahr = xmax[seq(length(xmax)-2)]*9/5 + 32,
+  Min_temp_Fahr = xmin[seq(length(xmin)-2)]*9/5 + 32
+)
